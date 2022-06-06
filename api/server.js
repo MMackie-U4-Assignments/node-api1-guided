c2102630369250023fcc9f674ba865e760f0d01a
James Blacklock <jamesblacklock@protonmail.com>
2022-06-06 13:52:17 -0400

    committing to "lecture"

--- api/server.js
+++ api/server.js
@@ -37,6 +37,11 @@ server.get('/api/dogs/:id', (req, res) => {
 });
 
 // [POST]   /api/dogs     (C of CRUD, create new dog from JSON payload)
+server.post('/api/dogs', (req, res) => {
+    console.log(req.body);
+    res.end();
+});
+
 // [PUT]    /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
 
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
